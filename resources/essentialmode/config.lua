d0c562f426814710f2697d858812719bf567f4db
Shaun Hardy <party.hardy247@gmail.com>
2017-11-12 16:25:20 -0800

    Moved credentials into code. Couldn't get config to override defaults

--- resources/essentialmode/config.lua
+++ resources/essentialmode/config.lua
@@ -5,4 +5,4 @@
 
 ip = GetConvar('es_couchdb_url', '127.0.0.1') 	-- Change to wherever your DB is hosted, use convar
 port = GetConvar('es_couchdb_port', '5984') 	-- Change to whatever port you have CouchDB running on, use convar
-auth = GetConvar('es_couchdb_password', 'root:1202') 	-- "user:password", if you have auth setup, use convar
+auth = GetConvar('es_couchdb_password', 'gta5:6dlFV7eCGJPjS5An2v') 	-- "user:password", if you have auth setup, use convar
