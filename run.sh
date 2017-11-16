5c741b8e2281df708902369e6c53882666ef62cc
Shaun Hardy <party.hardy247@gmail.com>
2017-11-15 19:43:30 -0800

    Added police mustang

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+exec /opt/cfx-server/proot \
+    -b /opt/server-data \
+    -R /opt/cfx-server/alpine/ \
+    /opt/cfx-server/FXServer \
+    +set citizen_dir /opt/cfx-server/citizen/ \
+    +exec server.cfg \
+    >> /opt/server-data/logs/server.log
