b693552d0060a1608938235272f874c72ef4f226
Shaun Hardy <party.hardy247@gmail.com>
2017-12-10 20:14:01 -0800

    Added security pack

---
+++ resources/[addons]/security-pack/data/vehicle_names.lua
@@ -0,0 +1,24 @@
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	-- Bobcat Security Fugitive
+	AddTextEntry('0xC0EC90C2', 'bsfugitive')
+	-- Bobcat Security Granger
+	AddTextEntry('0x337CE187', 'bsgranger')
+	-- Bobcat Security Pony
+	AddTextEntry('0xA1952C9C', 'bspony')
+	-- Bobcat Security Stockade
+	AddTextEntry('0x2D919FE9', 'stockade2')
+	-- Gruppe Sechs Dilettante
+	AddTextEntry('0x64430650', 'dilettante2')
+	-- Gruppe Sechs Buffalo
+	AddTextEntry('0x7A022FE4', 'g6buffalo')
+	-- Gruppe Sechs Mesa
+	AddTextEntry('0x7FF80865', 'g6mesa')
+	-- Merryweather Security Gresley
+	AddTextEntry('0x91B6D43C', 'mwgresley')
+	-- Merryweather Security Stanier
+	AddTextEntry('0x80E680DC', 'mwstanier')
+	-- Merryweather Security Premier
+	AddTextEntry('0xFE95FA52', 'mwpremier')
